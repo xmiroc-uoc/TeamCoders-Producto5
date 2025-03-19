78d0d45b0e870acace16f9f0b3b699f02b9afe5b
xmiroc <xmiroc@uoc.edu>
2025-03-19 19:55:53 +0100

    Creación inicial main

--- src/TeamCoders/Main.java
+++ src/TeamCoders/Main.java
@@ -1,10 +1,7 @@
 package TeamCoders;
 
-import TeamCoders.vista.TiendaView;
-
 public class Main{
     public static void main(String[] arg) {
-        TiendaView vista = new TiendaView();
-        vista.mostrarMenu();
+        
     }
 }
