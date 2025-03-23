19e4a10d5404d1c7a46042b1cfd2b81acf0107e5
xmiroc <xmiroc@uoc.edu>
2025-03-23 22:55:38 +0100

    Implementación de la clase Main - punto de entrada

--- src/TeamCoders/Main.java
+++ src/TeamCoders/Main.java
@@ -1,7 +1,10 @@
 package TeamCoders;
 
+import TeamCoders.vista.TiendaView;
+
 public class Main{
     public static void main(String[] arg) {
-        
+        TiendaView vista = new TiendaView();
+        vista.mostrarMenu();
     }
 }
