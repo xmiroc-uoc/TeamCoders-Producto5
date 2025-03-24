3cab1736c1f14129cfd21a29af08d68cc4dba0d9
unknown <msolerfor@uoc.edu>
2025-03-24 14:39:34 +0100

    Implementación de los métodos de pedidos

--- src/TeamCoders/controlador/Tienda.java
+++ src/TeamCoders/controlador/Tienda.java
@@ -34,6 +34,7 @@ public class Tienda {
                     break;
                 case "3":
                     System.out.println("Ha escogido Gestión de Pedidos\n");
+                    navegarPedidos(opcion);
                     break;
                 case "0":
                     break;
@@ -112,17 +113,17 @@ public class Tienda {
 
             switch (opcion) {
                 case "1":
-                    System.out.println("Ha escogido Añadir Pedido\n");
+                    agregarPedido();
                     break;
                 case "2":
-                    System.out.println("Ha escogido Eliminar Pedido\n");
+                    eliminarPedido();
                     break;
                 case "3":
-                    System.out.println("Ha escogido Mostrar Pedidos Pendientes\n");
+                    datos.mostrarPedidosPendientes();
                     System.out.print("\n");
                     break;
                 case "4":
-                    System.out.println("Ha escogido Mostrar Pedidos Enviados\n");
+                    datos.mostrarPedidosEnviados();
                     System.out.print("\n");
                     break;
                 case "0":
@@ -222,6 +223,120 @@ public class Tienda {
             Cliente newCliente = new ClientePremium(nombre, domicilio, nif, email, cuotaAnual);
             datos.agregarCliente(newCliente);
         }
+    }
+
+    public void agregarPedido(){
+        int numPedido;
+        int unidades;
+        Cliente clienteSeleccionado;
+        Articulo articuloSeleccionado;
+        
+
+        System.out.println("Introduzca numero de pedido\n");
+        numPedido = teclado.nextInt();
+        System.out.print("\n");
+
+        System.out.println("Introduzca el numero de unidades\n");
+        unidades = teclado.nextInt();
+        System.out.print("\n");
+
+        String opcion = "";
+        boolean opcionPosible = false;
+
+        while(!opcionPosible){
+            System.out.println("1. Seleccionar entre los clientes existentes\r\n" + //
+                            "2. Agregar nuevo cliente");
+            
+            System.out.print("\n");
+            teclado.nextLine();
+            opcion = teclado.nextLine();
+            System.out.print("\n");
+
+            if(opcion.equals("1") || opcion.equals("2")){
+                opcionPosible = true;
+            }else{
+                System.out.println("Escoja entre las opciones disponibles.");
+            }
+        }
+
+        if(opcion.equals("2")){
+            agregarCliente();
+            clienteSeleccionado = datos.getClientes().get((datos.getClientes().size())-1);
+        }else{
+            int opcionCliente = 0;
+            while(opcionPosible){
+                System.out.println("Seleccione un cliente");
+                datos.mostrarClientes();
+                System.out.println("\n");
+                
+                opcionCliente = teclado.nextInt();
+                if(opcionCliente > 0 && opcionCliente <= datos.getClientes().size()){
+                    opcionPosible = false;
+                }
+            }
+            clienteSeleccionado = datos.getClientes().get(opcionCliente - 1);
+        }
+
+        int opcionArticulo = 0;
+        boolean opcionArtPosible = false;
+
+        while(!opcionArtPosible){
+            System.out.println("Seleccione un articulo");
+            datos.mostrarArticulo();
+            System.out.println("\n");
+
+            opcionArticulo = teclado.nextInt();
+            if(opcionArticulo>0 && opcionArticulo <= datos.getArticulos().size()){
+                opcionArtPosible = true;
+            }else{
+                System.out.println("Escoja entre las opciones disponibles.");
+            }
+        }
+
+        articuloSeleccionado = datos.getArticulos().get(opcionArticulo - 1);
+
+        Date fecha = new Date();
+        Pedido newPedido = new Pedido(numPedido, unidades, fecha, clienteSeleccionado, articuloSeleccionado);
+        datos.agregarPedido(newPedido);
+    }
+
+    public void eliminarPedido(){
+        int opcionPedido = 0;
+        boolean opcionPedPosible = false;
+
+        while (!opcionPedPosible) {
+            System.out.println("Escoja el pedido que quiere eliminar");
+            datos.mostrarPedidos();
+            System.out.println("\n");
+
+            opcionPedido = teclado.nextInt();
+            if(opcionPedido>0 && opcionPedido <= datos.getPedidos().size()){
+                opcionPedPosible = true;
+                boolean eliminarPosible = checkPedidoEnviado(datos.getPedidos().get(opcionPedido-1));
+                if(eliminarPosible){
+                    datos.eliminarPedido(datos.getPedidos().get(opcionPedido - 1));
+                }else{
+                    System.out.println("Este pedido no se puede eliminar");
+                }
+                
+            }else{
+                System.out.println("Escoja entre las opciones disponibles.");
+            }
+
+        }
+    }
+
+    public boolean checkPedidoEnviado(Pedido pedido){
+        boolean eliminar = false;
+        Calendar calendar = Calendar.getInstance();
+        Date actualidad = new Date();
+        calendar.setTime(pedido.getFechaPedido());
+        calendar.add(Calendar.MINUTE, pedido.getArticulo().getTiempoPreparacion());
+        if(!calendar.getTime().before(actualidad)){
+            eliminar = true;
+        }
+
+        return eliminar;
     }
 
 }
--- src/TeamCoders/modelo/Datos.java
+++ src/TeamCoders/modelo/Datos.java
@@ -3,18 +3,30 @@ package modelo;
 import java.util.*;
 
 public class Datos{
-    private List<Articulo> articulos = new ArrayList<>();
-    private List<Cliente> clientes = new ArrayList<>();;
-    private List<Pedido> pedidos = new ArrayList<>();;
+    private ArrayList<Articulo> articulos = new ArrayList<>();
+    private ArrayList<Cliente> clientes = new ArrayList<>();;
+    private ArrayList<Pedido> pedidos = new ArrayList<>();;
 
     public Datos(){
         initialize();
     }
 
+    public ArrayList<Articulo> getArticulos(){
+        return articulos;
+    }
+
+    public ArrayList<Cliente> getClientes(){
+        return clientes;
+    }
+
+    public ArrayList<Pedido> getPedidos(){
+        return pedidos;
+    }
+
     public void initialize(){
         //Articulos
-        Articulo articulo1 = new Articulo("0001", 30, 12.5f, 50.0f, "Ejemplo Articulo 1");
-        Articulo articulo2 = new Articulo("0002", 10, 5f, 15f, "Ejemplo Articulo 2");
+        Articulo articulo1 = new Articulo("0001", 0, 12.5f, 50.0f, "Ejemplo Articulo 1");
+        Articulo articulo2 = new Articulo("0002", 1, 5f, 15f, "Ejemplo Articulo 2");
         Articulo articulo3 = new Articulo("0003", 20, 9.99f, 23.5f, "Ejemplo Articulo 3");
 
         articulos.add(articulo1);
@@ -44,8 +56,10 @@ public class Datos{
     }
 
     public void mostrarArticulo() {
+        int count = 1;
         for (Articulo a : articulos){
-            System.out.println(a.getDescripcion());
+            System.out.println(count + ". " + a.getDescripcion());
+            count ++;
         }
     }
 
@@ -54,23 +68,29 @@ public class Datos{
     }
 
     public void mostrarClientes() {
+        int count = 1;
         for (Cliente c : clientes){
-            System.out.println(c.getNombre());
+            System.out.println(count + ". " + c.getNombre());
+            count++;
         }
     }
 
     public void mostrarClientesEstandar(){
+        int count = 1;
         for (Cliente c : clientes){
             if(c instanceof ClienteEstandar){
-                System.out.println(c.getNombre());
+                System.out.println(count + ". " + c.getNombre());
+                count++;
             }
         }
     }
 
     public void mostrarClientesPremium(){
+        int count = 1;
         for (Cliente c : clientes){
             if(!(c instanceof ClienteEstandar)){
-                System.out.println(c.getNombre());
+                System.out.println(count + ". " + c.getNombre());
+                count++;
             }
         }
     }
@@ -84,8 +104,38 @@ public class Datos{
     }
 
     public void mostrarPedidos() {
+        int count = 1;
         for (Pedido p : pedidos){
-            System.out.println("Pedido #" + p.getNumeroPedido());
+            System.out.println(count + ". " + "Pedido #" + p.getNumeroPedido());
+            count++;
+        }
+    }
+
+    public void mostrarPedidosPendientes() {
+        int count = 1;
+        Calendar calendar = Calendar.getInstance();
+        Date actualidad = new Date();
+        for (Pedido p : pedidos){
+            calendar.setTime(p.getFechaPedido());
+            calendar.add(Calendar.MINUTE, p.getArticulo().getTiempoPreparacion());
+            if(!calendar.getTime().before(actualidad)){
+                System.out.println(count + ". " + "Pedido #" + p.getNumeroPedido());
+                count++;
+            }
+        }
+    }
+
+    public void mostrarPedidosEnviados() {
+        int count = 1;
+        Calendar calendar = Calendar.getInstance();
+        Date actualidad = new Date();
+        for (Pedido p : pedidos){
+            calendar.setTime(p.getFechaPedido());
+            calendar.add(Calendar.MINUTE, p.getArticulo().getTiempoPreparacion());
+            if(calendar.getTime().before(actualidad)){
+                System.out.println(count + ". " + "Pedido #" + p.getNumeroPedido());
+                count++;
+            }
         }
     }
 }
